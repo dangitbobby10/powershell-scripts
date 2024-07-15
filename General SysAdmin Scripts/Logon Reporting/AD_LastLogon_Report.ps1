458389c911f6965bc5ee1beac6d111845225cb42
dangitbobby10 <146766561+dangitbobby10@users.noreply.github.com>
2024-07-15 12:15:32 -0400

    july 2024 update

--- General SysAdmin Scripts/Logon Reporting/AD_LastLogon_Report.ps1
+++ General SysAdmin Scripts/Logon Reporting/AD_LastLogon_Report.ps1
@@ -1,68 +1,98 @@
-$session = New-PSSession -ComputerName "" #servername
-Invoke-Command -Session $session -ScriptBlock {
-    $logdate = get-date -f MM-dd-yyyy
-    $csvfile = "c:\users\$env:username\desktop\Roster_AD_Queries_AD_Only-$logdate.csv"
+$logdate = get-date -f MM-dd-yyyy
+$csvfile = "c:\temp\AD_LastLogon_Report-$logdate.csv" #update your path
 
-    # Get a list of every DC
-    $dcNames = Get-ADDomainController -Filter * |
-      Select-Object -ExpandProperty Name |
-      Sort-Object
+# Get a list of every DC
+$dcNames = Get-ADDomainController -Filter * |
+  Select-Object -ExpandProperty Name |
+  Sort-Object
 
-    # Filter OU and by Active Users
-    $searchBase = "DC=contoso,DC=com" #update to your domain
-    $users = Get-ADUser -Filter 'enabled -eq $true' -Properties * -SearchBase $searchBase | Where-Object {$_.info -NE 'Migrated'}
+# Filter OU
+$searchBase = "DC=contoso,DC=com" #update with your domain
 
-    $output = foreach ($user in $users) {
-        # Clear variables
-        $latestLogonFT = $latestLogonServer = $latestLogon = $null
-        $latestInteractiveLogonFT = $latestInteractiveLogon = $null  # Initialize variable for interactive logon time
+###########################################################################################################
+# Search All Users
+$users = Get-ADUser -Filter * -Properties * -SearchBase $searchBase | Where-Object {$_.info -NE 'Migrated'}
 
-        # Iterate every DC name
-        foreach ($dcName in $dcNames) {
-            # Query specific DC
-            $userDetails = Get-ADUser -Server $dcName -Identity $user.SamAccountName -Properties lastLogon, msDS-LastSuccessfulInteractiveLogonTime |
-                Select-Object -Property lastLogon, msDS-LastSuccessfulInteractiveLogonTime
+# Specify a single user
+#$users = Get-ADUser -Identity "test.user" -Properties *
+###########################################################################################################
 
-            $lastLogonFT = $userDetails.lastLogon
-            $interactiveLogonFT = $userDetails.'msDS-LastSuccessfulInteractiveLogonTime'
+# Hashtable used for splatting for Get-ADUser in loop
+$params = @{
+  "Properties" = @("lastLogon", "msDS-LastSuccessfulInteractiveLogonTime")
+}
 
-            # Remember most recent file time and DC name for lastLogon
-            if ($lastLogonFT -and ($lastLogonFT -gt $latestLogonFT)) {
-                $latestLogonFT = $lastLogonFT
-                $latestLogonServer = $dcName
-            }
-            # Check for the most recent interactive logon time
-            if ($interactiveLogonFT -and ($interactiveLogonFT -gt $latestInteractiveLogonFT)) {
-                $latestInteractiveLogonFT = $interactiveLogonFT
-            }
-        }
+$output = foreach ( $user in $users ) {
+  # Set LDAPFilter to find specific user
+  $params.LDAPFilter = "(sAMAccountName=$($user.SamAccountName))"
 
-        if ($latestLogonFT -and ($latestLogonFT -gt 0)) {
-            # If user ever logged on, get DateTime from file time
-            $latestLogon = [DateTime]::FromFileTime($latestLogonFT)
-        } else {
-            # User never logged on
-            $latestLogon = $latestLogonServer = $null
-        }
+  # Clear variables
+  $latestLogonFT = $latestLogonServer = $latestLogon = $null
+  $latestInteractiveLogonFT = $latestInteractiveLogon = $null
 
-        if ($latestInteractiveLogonFT -and ($latestInteractiveLogonFT -gt 0)) {
-            $latestInteractiveLogon = [DateTime]::FromFileTime($latestInteractiveLogonFT)
-        }
+  # Iterate every DC name
+  foreach ( $dcName in $dcNames ) {
+    # Query specific DC
+    $params.Server = $dcName
 
-        # Output User Data
-        $user | Select-Object `
-            @{Label = "Display Name"; Expression = {$_.displayname}},
-            @{Name = "LatestLogon"; Expression = {$latestLogon}},
-            @{Name = "LatestLogonServer"; Expression = {$latestLogonServer}},
-            @{Name = "LatestInteractiveLogon"; Expression = {$latestInteractiveLogon}},  # Add interactive logon time
-            @{Label = "User Access Control"; Expression = {$_.userAccountControl}},
-            @{Label = "User Principal Name"; Expression = {$_.userPrincipalName}},
-            @{Label = "Employee ID"; Expression = {$_.employeeID}},
-            @{Label = "First Name"; Expression = {$_.GivenName}},
-            @{Label = "Last Name"; Expression = {$_.Surname}},
-            @{Label = "Manager"; Expression = {%{(Get-AdUser $_.Manager -server $dcName -Properties DisplayName).DisplayName}}},
-            @{Label = "Organizational Unit"; Expression = {$_.distinguishedName}}
+    # Get lastLogon and msDS-LastSuccessfulInteractiveLogonTime attributes
+    $userDetails = Get-ADUser @params |
+      Select-Object -Property lastLogon, msDS-LastSuccessfulInteractiveLogonTime
+
+    $lastLogonFT = $userDetails.lastLogon
+    $interactiveLogonFT = $userDetails.'msDS-LastSuccessfulInteractiveLogonTime'
+
+    # Remember most recent file time and DC name for lastLogon
+    if ( $lastLogonFT -and ($lastLogonFT -gt $latestLogonFT) ) {
+      $latestLogonFT = $lastLogonFT
+      $latestLogonServer = $dcName
     }
 
-    $output | Export-Csv -Path $csvfile -NoTypeInformation
+    # Check for the most recent interactive logon time
+    if ($interactiveLogonFT -and ($interactiveLogonFT -gt $latestInteractiveLogonFT)) {
+      $latestInteractiveLogonFT = $interactiveLogonFT
+    }
+  }
+
+  if ( $latestLogonFT -and ($latestLogonFT -gt 0) ) {
+    # If user ever logged on, get DateTime from file time
+    $latestLogon = [DateTime]::FromFileTime($latestLogonFT)
+  }
+  else {
+    # User never logged on
+    $latestLogon = $latestLogonServer = $null
+  }
+
+  if ($latestInteractiveLogonFT -and ($latestInteractiveLogonFT -gt 0)) {
+    $latestInteractiveLogon = [DateTime]::FromFileTime($latestInteractiveLogonFT)
+  }
+
+  # Get lastLogonTimestamp attribute
+  $lastLogonTimestamp = $user.lastLogonTimestamp
+
+  # Convert lastLogonTimestamp to DateTime if it's not null
+  if ($lastLogonTimestamp -ne $null) {
+    $lastLogonTimestamp = [DateTime]::FromFileTime($lastLogonTimestamp)
+  }
+
+  # Output User Data
+  $user | Select-Object `
+    @{Label = "Display Name";Expression = {$_.name}},
+    @{Label = "Password Last Set";Expression = {$_.PasswordLastSet}},
+    @{Label = "Password Expires";Expression = {if ($_.PasswordNeverExpires -eq $false) {$_.PasswordLastSet.AddDays(90)} else {"Never"}}}, # Assuming password expires in 90 days
+    @{Label = "LatestLogon";Expression = {$latestLogon}},
+    @{Label = "LatestLogonServer"; Expression = {$latestLogonServer}},
+    @{Label = "LastLogonTimestamp"; Expression = {$lastLogonTimestamp}},
+    @{Label = "LatestInteractiveLogon"; Expression = {$latestInteractiveLogon}},  # Add interactive logon time
+    @{Label = "User Principal Name";Expression = {$_.userPrincipalName}},
+    @{Label = "Pre-Windows 2000 Username";Expression = {$_.sAMAccountName}},
+    @{Label = "Employee ID";Expression = {$_.employeeID}},
+    @{Label = "First Name";Expression = {$_.GivenName}},
+    @{Label = "Last Name";Expression = {$_.Surname}},
+    @{Label = "Manager";Expression = {%{(Get-AdUser $_.Manager -Properties DisplayName).DisplayName}}},
+    @{Label = "Distinguished Name";Expression = {$_.DistinguishedName}},
+    @{Label = "Organizational Unit";Expression = {($_.DistinguishedName -split '(?<=,)',2)[1] -replace 'DC=','' -replace ',','.'}},
+    @{Label = "Account Status";Expression = {if ($_.Enabled -eq $true) {"Enabled"} else {"Disabled"}}}
 }
+
+$output | Export-Csv -Path $csvfile -NoTypeInformation
